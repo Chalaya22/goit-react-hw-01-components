2507dbd1a5f5e2fbe919fe39a3fc6a4fc6122c00
Chalaya22 <chalayaolga22@gmail.com>
2023-10-13 01:06:44 +0300

    comit 10

--- src/components/Transaction/TransactionHistory.jsx
+++ src/components/Transaction/TransactionHistory.jsx
@@ -1,21 +1,23 @@
+import css from './TransactionHistory.module.css';
+
 const TransactionHistory = ({ items }) => {
   return (
-    <table className="transaction-history">
-      <thead className="t-head">
-        <tr className="t-head-row">
-          <th>Type</th>
-          <th>Amount</th>
-          <th>Currency</th>
+    <table className={css.transactionTablle}>
+      <thead className={css.tabHead}>
+        <tr className={css.tabHeadRow}>
+          <th className={css.type}>Type</th>
+          <th className={css.amount}>Amount</th>
+          <th className={css.currency}>Currency</th>
         </tr>
       </thead>
 
-      <tbody className="t-body">
+      <tbody className={css.tableBody}>
         {items.map(({ id, type, amount, currency }) => {
           return (
-            <tr className="t-body-row" key={id}>
-              <td>{type}</td>
-              <td>{amount}</td>
-              <td>{currency}</td>
+            <tr className={css.tableBodyRow} key={id}>
+              <td className={css.typeBody}>{type}</td>
+              <td className={css.amountBody}>{amount}</td>
+              <td className={css.currensyBody}>{currency}</td>
             </tr>
           );
         })}
